579da1ffe8dfd22ce729744087ecf53b026f34e0
junyuchen245 <ericchan000@gmail.com>
2025-05-19 21:25:08 -0400

    wrapper for convexAdam

--- models/convexAdam/configs_ConvexAdam_MIND.py
+++ models/convexAdam/configs_ConvexAdam_MIND.py
@@ -20,3 +20,24 @@ def get_ConvexAdam_MIND_brain_default_config():
     config.result_path = './'
     config.verbose = False
     return config
+
+def get_ConvexAdam_MIND_lung_default_config():
+    '''
+    ConvexAdam MIND default config.
+    '''
+    config = ml_collections.ConfigDict()
+    config.mind_r = 1
+    config.mind_d = 2
+    config.lambda_weight = 0.65
+    config.grid_sp = 4
+    config.disp_hw = 6
+    config.selected_niter = 80
+    config.selected_smooth = 1
+    config.grid_sp_adam = 2
+    config.ic = True
+    config.use_mask = False
+    config.path_fixed_mask = None
+    config.path_moving_mask = None
+    config.result_path = './'
+    config.verbose = False
+    return config
