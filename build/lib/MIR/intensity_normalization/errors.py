21a9b2cf9f807d1237bae0b462c709ed8b9b0bb3
junyuchen245 <ericchan000@gmail.com>
2025-05-19 21:53:57 -0400

    Ignore build/, dist/, and egg-info folders

--- build/lib/MIR/intensity_normalization/errors.py
+++
@@ -1,8 +0,0 @@
-"""Project-specific errors
-Author: Jacob Reinhold <jcreinhold@gmail.com>
-Created on: 24 Apr 2018
-"""
-
-
-class NormalizationError(RuntimeError):
-    pass
--- build/lib/MIR/models/convexAdam/__init__.py
+++
@@ -1,9 +0,0 @@
-from .convex_adam_MIND import convex_adam_MIND
-from .convex_adam_nnUNet import convex_adam_seg_features
-from .configs_ConvexAdam_MIND import get_ConvexAdam_MIND_brain_default_config, get_ConvexAdam_MIND_lung_default_config
-__all__ = [
-    'convex_adam_MIND',
-    'convex_adam_seg_features',
-    'get_ConvexAdam_MIND_brain_default_config',
-    'get_ConvexAdam_MIND_lung_default_config'
-]
